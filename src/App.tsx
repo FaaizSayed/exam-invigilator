0ab24c3dd23efe994cd3e3b98a980044d4dee793
Sayed Faaiz Shah <faaiz.shah@gmail.com>
2025-06-29 06:39:35 +0400

    Removed unnecessary imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import DownloadedAssessments from "./pages/DownloadedAssessments";
 import TrackExamSubmissions from "./pages/TrackExamSubmissions";
